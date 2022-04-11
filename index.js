13b94cb7ecc7c7af6affffa011a5614985f6145e
Johnny Chen <johnnyc1314@gmail.com>
2022-04-10 23:49:02 -0400

    commit tree 1204c4ad98f064c94b34acc5dd0eb3b2113d5cb9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = (i + 1).toString();
+//     ul.append(li)
+// }
+
+/* Enter the code to remove the node element here */
+const main = document.querySelector('main')
+main.remove()
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Johnny is the champion'
+
+document.body.append(newHeader)
